913befe783195666df46e639422a490eae30050d
SeukWon Kang <kasworld@gmail.com>
2024-10-26 15:33:12 +0900

    이동경로 추가

--- 윷놀이.gd
+++ 윷놀이.gd
@@ -7,7 +7,14 @@ var vp_size :Vector2
 func _ready() -> void:
 	vp_size = get_viewport_rect().size
 	var r = min(vp_size.x,vp_size.y)/2 *0.9
+	var sh = r/20
+	판추가(r,Color.RED,Vector2(-sh,-sh))
+	판추가(r,Color.BLUE,Vector2(-sh,sh))
+	판추가(r,Color.GREEN,Vector2(sh,-sh))
+	판추가(r,Color.YELLOW,Vector2(sh,sh))
+
+func 판추가(r:float, co :Color,shift :Vector2):
 	var 이번판 = 판_scene.instantiate()
-	이번판.init(r, Color.RED)
-	이번판.position = vp_size/2
+	이번판.init(r, co)
+	이번판.position = vp_size/2 + shift
 	add_child(이번판)
--- 판.gd
+++ 판.gd
@@ -2,7 +2,14 @@ extends Node2D
 
 var 눈_scene = preload("res://눈.tscn")
 
+var r :float
+var co :Color
+var 화살표선들 :PackedVector2Array =[]
+
+
 func init(r: float, co :Color) -> void:
+	self.r = r
+	self.co = co
 	for i in range(0,360,360/20):
 		var rd = deg_to_rad(i)
 		var pos = make_pos_by_rad_r(rd,r)
@@ -14,6 +21,13 @@ func init(r: float, co :Color) -> void:
 
 	눈추가(r,Vector2(0,0),co)
 
+	for i in range(0,360,360/20):
+		var rd1 = deg_to_rad(i)
+		var rd2 = deg_to_rad(i+360/20)
+		화살표선들.append_array([make_pos_by_rad_r(rd1,r),make_pos_by_rad_r(rd2,r)])
+	for i in [-1.0,-0.33,-0.66,0,0.33,0.66]:
+		화살표선들.append_array([Vector2(r*i,0),Vector2(r*(i+0.33),0)])
+		화살표선들.append_array([Vector2(0,r*i),Vector2(0,r*(i+0.33))])
 
 func 눈추가(r: float, pos:Vector2,co:Color):
 	var 눈1 = 눈_scene.instantiate()
@@ -21,6 +35,8 @@ func 눈추가(r: float, pos:Vector2,co:Color):
 	눈1.position = pos
 	add_child(눈1)
 
+func _draw() -> void:
+	draw_multiline(화살표선들,co, 1)
 
 
 func _ready() -> void:
