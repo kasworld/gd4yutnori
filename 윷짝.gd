55d0abbf2825e09d3ba4fd6f1a49f9eb96073f33
SeukWon Kang <kasworld@gmail.com>
2024-10-28 10:41:29 +0900

    윷짝 분리

--- 윷짝.gd
+++ 윷짝.gd
@@ -2,12 +2,29 @@ extends Node2D
 class_name 윷짝
 
 var 윷_scene = preload("res://윷.tscn")
-
+var 윷들 :Array[윷]
 func init()->윷짝:
+	for i in ["뒷도","뒷개","뒷걸","도","결과"]:
+		var lb = Label.new()
+		lb.label_settings = preload("res://label_settings.tres")
+		lb.text = i
+		$"윷통".add_child(lb)
+
 	for i in range(0,4):
-		$"윷통".add_child(윷_scene.instantiate().init())
+		var n = 윷_scene.instantiate().init()
+		$"윷통".add_child(n)
+		윷들.append(n)
+
+	var lb = Label.new()
+	lb.label_settings = preload("res://label_settings.tres")
+	lb.text = "모"
+	$"윷통".add_child(lb)
+
 	return self
 
-func 윷던지기():
-	for n in $"윷통".get_children():
-		n.던지기()
+func 윷던지기()->Array[int]:
+	var 결과 :Array[int]
+	for n in 윷들:
+		결과.append( n.던지기() )
+	print(결과)
+	return 결과
