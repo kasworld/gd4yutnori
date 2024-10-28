681d8c9c9df9fa44716473843da793854bf43eaf
SeukWon Kang <kasworld@gmail.com>
2024-10-28 15:33:51 +0900

    여러시작점

--- 말이동길.gd
+++ 말이동길.gd
@@ -6,40 +6,73 @@ var w :float
 var co :Color
 var 화살표선들 :PackedVector2Array =[]
 
+# 눈번호
 var 바깥길 :Array[int]
 var 첫지름길 :Array[int]
 var 둘째지름길 :Array[int]
 var 세째지름길 :Array[int]
 
-func init(r: float, co :Color) -> void:
+func init(r: float, co :Color, 시작눈 :int, 눈들 :Array[눈]) -> void:
 	self.r = r
 	self.co = co
 	self.w = max(1.0, r/300)
 
-	var 눈각도 = 360/20
-
 	# 말 이동 순서 연결하기
-	바깥길 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
-	첫지름길 = [4,20,21,22,23,24,14]
-	둘째지름길 = [9,25,26,22,27,28,19]
-	세째지름길 = [22,27,28,19]
+	match 시작눈:
+		0:
+			바깥길 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+			첫지름길 = [4,20,21,22,23,24,14]
+			둘째지름길 = [9,25,26,22,27,28,19]
+			세째지름길 = [22,27,28,19]
+		5:
+			바깥길 = [5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,0,1,2,3,4]
+			첫지름길 = [9,25,26,22,27,28,19]
+			둘째지름길 = [14,24,23,22,21,20,4]
+			세째지름길 = [22,21,20,4]
+		10:
+			바깥길 = [10,11,12,13,14,15,16,17,18,19,0,1,2,3,4,5,6,7,8,9]
+			첫지름길 = [14,24,23,22,21,20,4]
+			둘째지름길 = [19,28,27,22,26,25,9]
+			세째지름길 = [22,26,25,9]
+		15:
+			바깥길 = [15,16,17,18,19,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+			첫지름길 = [19,28,27,22,26,25,9]
+			둘째지름길 = [4,20,21,22,23,24,14]
+			세째지름길 = [22,23,24,14]
 
-	# 눈 연결선 그리기
-	for i in range(눈각도,360,눈각도):
-		var rd1 = deg_to_rad(i)
-		var rd2 = deg_to_rad(i+눈각도)
-		화살표추가(make_pos_by_rad_r(rd1,r),make_pos_by_rad_r(rd2,r))
-	#화살표추가(make_pos_by_rad_r(0,r),make_pos_by_rad_r(0,r*2))
+		_:
+			print("잘못된 시작점입니다.0,5,10,15만 가능")
+			get_tree().quit()
 
-	var th = 1.0/3.0
-	화살표추가( Vector2(0,r), Vector2(-th*r,r*(1-th)) )
-	화살표추가( Vector2(th*r,r*(1-th)), make_pos_by_rad_r(deg_to_rad(눈각도),r) )
+	for i in range(0,바깥길.size()-1):
+		var p1 = 눈들[바깥길[i]].position
+		var p2 = 눈들[바깥길[i+1]].position
+		화살표추가(p1,p2)
 
-	for i in [-1.0,-th*2,-th,0,th,th*2]:
-		# 세로 화살표
-		화살표추가(Vector2(0,r*i),Vector2(0,r*(i+th)))
-		# 가로 화살표
-		화살표추가(Vector2(r*(i+th),0),Vector2(r*i,0))
+	for i in range(0,첫지름길.size()-1):
+		var p1 = 눈들[첫지름길[i]].position
+		var p2 = 눈들[첫지름길[i+1]].position
+		화살표추가(p1,p2)
+
+	for i in range(0,둘째지름길.size()-1):
+		var p1 = 눈들[둘째지름길[i]].position
+		var p2 = 눈들[둘째지름길[i+1]].position
+		화살표추가(p1,p2)
+
+	var 중점 = 눈들[22].position
+
+	var 시작점 = 눈들[바깥길[0]].position
+	var 시작점0 = ((중점-시작점)*0.3).rotated(-PI/6) + 시작점
+	화살표추가(시작점0,시작점)
+
+	var 끝점 = 눈들[바깥길[-1]].position
+	var 끝점0 = ((중점-끝점)*0.3).rotated(-PI/6) + 끝점
+	화살표추가(끝점,끝점0)
+
+	#var th = 1.0/3.0
+	#화살표추가( Vector2(0,r), Vector2(-th*r,r*(1-th)) )
+	#화살표추가( Vector2(th*r,r*(1-th)), make_pos_by_rad_r(deg_to_rad(눈각도),r) )
+#
 
 func 화살표추가(p1 :Vector2, p2 :Vector2):
 	var t1 = (p2-p1)*0.8+p1
