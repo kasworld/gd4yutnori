e09ad124ce3bcc4fa31df229da74a889e74e692e
SeukWon Kang <kasworld@gmail.com>
2024-10-29 16:28:33 +0900

    말통들 모이기

--- 윷놀이.gd
+++ 윷놀이.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 var 편_scene = preload("res://편.tscn")
-var 말_scene = preload("res://말.tscn")
 var 말이동길_scene = preload("res://말이동길.tscn")
 
 var vp_size :Vector2
@@ -11,6 +10,9 @@ var 말이동길들 :Array[말이동길]
 
 var 편들 :Array[편]
 
+@onready var 달말통 = $"판밖말들/HBoxContainer/PanelContainer/VBoxContainer/VBoxContainer"
+@onready var 난말통 = $"판밖말들/HBoxContainer/PanelContainer2/VBoxContainer2/VBoxContainer"
+
 func _ready() -> void:
 	vp_size = get_viewport_rect().size
 	var r = min(vp_size.x,vp_size.y)/2 *0.9
@@ -19,18 +21,20 @@ func _ready() -> void:
 	$"말눈들".position = vp_size/2
 	$"윷짝".init()
 	$"윷짝".position = vp_size/2 + Vector2(-r/2.3,-r/3)
-	$"달말들".position = vp_size/2 + Vector2(r/3,r/3)
-	$"난말들".position = vp_size/2 + Vector2(-r/3,r/3)
 	$"윷던지기".position = vp_size/2 + Vector2(r/8,-r/2)
+	$"판밖말들".position = vp_size/2 + Vector2(-r/2,r/4)
 
 	for co in 편색들:
 		말이동길추가(r,co)
 
 	for c in 편색들:
 		var t = 편_scene.instantiate()
-		t.init(NamedColorList.get_colorname_by_color(c), 4, r/30, c)
+		var n1 = HBoxContainer.new()
+		달말통.add_child(n1)
+		var n2 = HBoxContainer.new()
+		난말통.add_child(n2)
+		t.init(NamedColorList.get_colorname_by_color(c), 4, r/30, c,n1,n2)
 		편들.append(t)
-		#add_child(t)
 
 	for t in 편들:
 		while true:
@@ -38,16 +42,11 @@ func _ready() -> void:
 			if m == null:
 				break
 			var n = $"말눈들".눈얻기(randi_range(0,28))
-			print("놓을눈 ",n)
 			var oldms = n.말놓기([m])
 			for om in oldms:
-				print("잡힌말 ", om)
 				om.편얻기().놓을말되돌려넣기(om)
 
-	#for t in 편들:
-		#for i in range(0,20):
-			#$"말눈들".눈얻기(randi_range(0,28)).말놓기([말_scene.instantiate().init(t, r/30, t.편색, randi_range(1,4) )])
-
+	말이동길보이기변경()
 
 func _on_윷던지기_pressed() -> void:
 	$"윷짝".윷던지기()
@@ -69,7 +68,3 @@ func 말이동길보이기변경():
 	지금보이는말이동길번호 +=1
 	지금보이는말이동길번호 %= 말이동길들.size()
 	말이동길들[지금보이는말이동길번호].visible = true
-
-func _on_timer_timeout() -> void:
-	return
-	말이동길보이기변경()
--- 편.gd
+++ 편.gd
@@ -7,22 +7,31 @@ var 편이름 :String
 var 편색 :Color
 var 놓을말 :Array[말]
 var 난말 :Array[말]
+var 놓을말통 :HBoxContainer
+var 난말통 :HBoxContainer
 
-func init(이름:String, 말수 :int, r:float, co:Color) -> void:
+func init(이름:String, 말수 :int, r:float, co:Color, 달통 :HBoxContainer, 날통:HBoxContainer) -> void:
 	편이름 = 이름
 	편색 = co
+	놓을말통 = 달통
+	난말통 = 날통
 	for i in range(0,말수):
 		var m = 말_scene.instantiate().init(self, r, co, i+1)
 		놓을말.append(m)
+		놓을말통.add_child(m)
 
 func 놓을말얻기()->말:
 	if 놓을말.size() == 0:
 		print("놓을말이 없습니다.", 편이름)
 		return null
-	return 놓을말.pop_front()
+	var m = 놓을말.pop_front()
+	놓을말통.remove_child(m)
+	return m
 
 func 놓을말되돌려넣기(m :말):
 	놓을말.push_back(m)
+	놓을말통.add_child(m)
 
 func 난말넣기(m :말):
 	난말.push_back(m)
+	난말통.add_child(m)
