424683cbceb3b37005ed1ac89654ea5768f90ecd
SeukWon Kang <kasworld@gmail.com>
2024-11-28 13:32:07 +0900

    update arrow

--- arrow/arrow.gd
+++ arrow/arrow.gd
@@ -4,13 +4,17 @@ class_name Arrow
 func init_vector(vt :Vector2, co :Color, w :float)->Arrow:
 	return init_2_point(Vector2.ZERO, vt, co, w)
 
+func init_2_point_center(p1 :Vector2, p2 :Vector2, co :Color, w :float)->Arrow:
+	var c = (p1+p2)/2
+	return init_2_point(p1-c, p2-c, co, w)
+
 func init_2_point(p1 :Vector2, p2 :Vector2, co :Color, w :float)->Arrow:
-	$CenterLine.default_color = co
-	$LeftLine.default_color = co
-	$RightLine.default_color = co
-	$CenterLine.width = w
-	$LeftLine.width = w
-	$RightLine.width = w
+	set_color(co)
+	set_width(w)
+	set_points(p1,p2)
+	return self
+
+func set_points(p1 :Vector2, p2 :Vector2):
 	$CenterLine.add_point(p1)
 	$CenterLine.add_point(p2)
 	var p3 = ((p1-p2)*0.2).rotated(PI/6) + p2
@@ -19,8 +23,13 @@ func init_2_point(p1 :Vector2, p2 :Vector2, co :Color, w :float)->Arrow:
 	var p4 = ((p1-p2)*0.2).rotated(-PI/6) + p2
 	$RightLine.add_point(p4)
 	$RightLine.add_point(p2)
-	return self
 
-func init_2_point_center(p1 :Vector2, p2 :Vector2, co :Color, w :float)->Arrow:
-	var c = (p1+p2)/2
-	return init_2_point(p1-c, p2-c, co, w)
+func set_color(co :Color):
+	$CenterLine.default_color = co
+	$LeftLine.default_color = co
+	$RightLine.default_color = co
+
+func set_width(w :float):
+	$CenterLine.width = w
+	$LeftLine.width = w
+	$RightLine.width = w
