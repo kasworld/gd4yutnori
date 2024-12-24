640bc7a60d20cced82446e1d05e22316de994efd
SeukWon Kang <kasworld@gmail.com>
2024-12-24 11:55:04 +0900

    번호 없는 말

--- 말.gd
+++ 말.gd
@@ -15,14 +15,15 @@ func string_debug() -> String:
 func _to_string() -> String:
 	return "%s말%d" % [속한편,말번호]
 
-func init(t :편, r :float, n:int) -> 말:
+func init(t :편, r :float, n:int, hide_num :bool = false) -> 말:
 	속한편 = t
 	말번호 = n
 	custom_minimum_size = Vector2(r*2,r*2)
 	r = r*1.0 * t.인자.크기보정
 	PolygonNode.alter_polygon_fill($"내부", Vector2(r,r),r,t.인자.색,t.인자.모양,360)
 	PolygonNode.alter_polygon($"테두리", Vector2(r,r),r*1.1,Color.BLACK,max(1,r/10),t.인자.모양,360)
-	$"번호".text = "%d" % 말번호
+	if not hide_num:
+		$"번호".text = "%d" % 말번호
 	return self
 
 func 편얻기()->편:
