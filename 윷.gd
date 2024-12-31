955fd356cdddea9c55ba5b164ab2976332de1607
SeukWon Kang <kasworld@gmail.com>
2024-12-31 12:35:12 +0900

    윷 애니메니션 개선

--- 윷.gd
+++ 윷.gd
@@ -1,30 +1,19 @@
 extends Control
 class_name 윷
 
-const ANI_SEC = 0.1
-var ani_start :float
 var 결과 :int
 
 func init() -> 윷:
 	custom_minimum_size = Vector2(36,64)*2
-	$"배면".position = custom_minimum_size/2
-	$"등면".position = custom_minimum_size/2
+	$AnimatedSprite2D.position = custom_minimum_size/2
 	return self
 
 func 던지기() -> int:
 	결과 = randi_range(0,1)
-	결과표시()
-	ani_start = Time.get_unix_time_from_system()
+	$AnimatedSprite2D.play("new_animation")
+	$Timer.start(0.5)
 	return 결과
 
-func 결과표시():
-	$"배면".visible = 결과 == 1
-	$"등면".visible = 결과 == 0
-
-func _process(_delta: float) -> void:
-	var dur = Time.get_unix_time_from_system() - ani_start
-	if dur > ANI_SEC:
-		결과표시()
-		return
-	$"배면".visible = not $"배면".visible
-	$"등면".visible = not $"등면".visible
+func _on_timer_timeout() -> void:
+	$AnimatedSprite2D.stop()
+	$AnimatedSprite2D.frame = 결과
